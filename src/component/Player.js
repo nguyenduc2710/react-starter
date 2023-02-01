e1b1a4002013879a03430192deae0b2f8cc19fc8
NHHD-Yamj <hongducnguyenho0@gmail.com>
2023-02-01 13:36:38 +0700

    Functional Components

---
+++ src/component/Player.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Players() {
+    return (
+        <div className='container'>
+            <h2>This is Players section, and it broken</h2>
+        </div>
+    )
+}
